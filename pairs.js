accd5e65477437385f1767f59f14c9c1946c0305
Collably Network <165827418+Collablynetwork@users.noreply.github.com>
2025-04-20 10:51:45 +0530

    Update pairs.js

--- pairs.js
+++ pairs.js
@@ -1,4 +1,13 @@
+// pairs.js
+// -------------------------------------------------------------------------------------------------
+// List of USDT trading pairs to track.
+// -------------------------------------------------------------------------------------------------
+
 export const trackedPairs = [
-  "BTCUSDT","ETHUSDT","XRPUSDT","SOLUSDT","ADAUSDT","DOTUSDT","LTCUSDT","LINKUSDT","AVAXUSDT",
-  "ATOMUSDT","MATICUSDT","ARBUSDT","NEARUSDT","OPUSDT","INJUSDT","APTUSDT","AAVEUSDT","ETCUSDT","FILUSDT","HBARUSDT",
-  "EGLDUSDT","STXUSDT","XLMUSDT","TONUSDT","ALGOUSDT","RUNEUSDT","SUIUSDT","FLOWUSDT","DYDXUSDT"];
+  "BTCUSDT", "ETHUSDT", "XRPUSDT", "SOLUSDT", "ADAUSDT",
+  "DOTUSDT", "LTCUSDT", "LINKUSDT", "AVAXUSDT", "ATOMUSDT",
+  "POLUSDT", "ARBUSDT", "NEARUSDT", "OPUSDT", "INJUSDT",
+  "APTUSDT", "AAVEUSDT", "ETCUSDT", "FILUSDT", "HBARUSDT",
+  "EGLDUSDT", "STXUSDT", "XLMUSDT", "TONUSDT", "ALGOUSDT",
+  "RUNEUSDT", "SUIUSDT", "DYDXUSDT",
+];
